ee8df30d0227af46bbba64509030d90d6bb20733
WyyzaPd <42916553+GzSource@users.noreply.github.com>
2020-02-21 10:02:47 +0800

    Updated skill content to add Silence
    Updated skill content to add Silence

--- Content/LeagueSandbox-Scripts/Champions/Kassadin/Q.cs
+++ Content/LeagueSandbox-Scripts/Champions/Kassadin/Q.cs
@@ -1,6 +1,8 @@
 using GameServerCore.Enums;
 using GameServerCore.Domain.GameObjects;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.GameObjects.Missiles;
 using GameServerCore.Domain;
 using LeagueSandbox.GameServer.Scripting.CSharp;
 
@@ -18,7 +20,7 @@ namespace Spells
 
         public void OnStartCasting(IObjAiBase owner, ISpell spell, IAttackableUnit target)
         {
-            AddParticleTarget(owner, "Kassadin_Base_cas.troy", owner, 1, "L_HAND");
+            AddParticleTarget(owner, "Kassadin_Base_Q_cas.troy", owner, 1, "L_HAND");
         }
 
         public void OnFinishCasting(IObjAiBase owner, ISpell spell, IAttackableUnit target)
@@ -28,18 +30,14 @@ namespace Spells
 
         public void ApplyEffects(IObjAiBase owner, IAttackableUnit target, ISpell spell, IProjectile projectile)
         {
-            var ap = owner.Stats.AbilityPower.Total * 0.7f;
-            var damage = 30 + spell.Level * 50 + ap;
-
+            var AbilityPower = owner.Stats.AbilityPower.Total * 0.7f;
+            var SilenceTime = new[] { 0, 1.5f, 1.75f, 2, 2.25f, 2.5f }[spell.Level];
+            var Damage = new[] { 0, 80, 130, 180, 230, 280 }[spell.Level] + AbilityPower;
             if (target != null && !target.IsDead)
             {
-                target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL,
-                    false);
-                if (target.IsDead)
-                {
-                }
+                target.TakeDamage(owner, Damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL,false);
+                AddBuff("Silence", SilenceTime, 1, spell, (IObjAiBase)target, owner);
             }
-
             projectile.SetToRemove();
         }
 
